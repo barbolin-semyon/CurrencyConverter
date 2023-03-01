0a4c21846b0a046533072b589ff3bac795fee0f9
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-01 23:13:06 +0300

    Создание RetrofitClient.kt и RetrofitService.kt

---
+++ app/src/main/java/com/skat/currencyconverter/model/networkService/RetrofitClient.kt
@@ -0,0 +1,19 @@
+package com.skat.currencyconverter.model.networkService
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private var retrofitService: Retrofit? = null
+
+    fun getRetrofitService(): Retrofit {
+        if (retrofitService == null) {
+            retrofitService = Retrofit.Builder()
+                .baseUrl("https://www.cbr-xml-daily.ru/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        return retrofitService!!
+    }
+}
