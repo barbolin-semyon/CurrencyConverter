937e3ea672233fcf1911cbf8a3e2ce7dc682a61e
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-01 21:51:32 +0300

    Установка цвета

--- app/src/main/java/com/example/currencyconverter/ui/theme/Color.kt
+++ app/src/main/java/com/example/currencyconverter/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.example.currencyconverter.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Gray = Color(0xFFC5C5C5)
+val DarkGray = Color(0xFF1C3B53)
+val Green = Color(0xFFD2ECDA)
