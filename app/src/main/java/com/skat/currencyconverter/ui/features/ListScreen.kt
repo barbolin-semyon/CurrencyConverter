eb6d0eb266cc55568e28d9d42e9f97623cd96bce
barbolin-semyon <skat.apps@gmail.com>
2023-03-05 14:25:48 +0300

    Добавление кеширования

--- app/src/main/java/com/skat/currencyconverter/ui/features/ListScreen.kt
+++ app/src/main/java/com/skat/currencyconverter/ui/features/ListScreen.kt
@@ -1,5 +1,6 @@
 package com.skat.currencyconverter.ui.features
 
+import android.content.Context.MODE_PRIVATE
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,29 +15,38 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.skat.currencyconverter.model.emptities.currenciesName
 import com.skat.currencyconverter.ui.theme.DarkGray
+import com.skat.currencyconverter.ui.theme.Green
 import com.skat.currencyconverter.viewModel.CurrencyViewModel
 
 @Composable
 fun ListScreen(navController: NavController, viewModel: CurrencyViewModel) {
     val currencies by viewModel.currencies.observeAsState()
+    val context = LocalContext.current
 
     currencies?.let {
         LazyColumn(verticalArrangement = Arrangement.spacedBy(16.dp)) {
             items(it.keys.toList()) { code ->
-                ItemCurrency(code = code, currencies!![code]!!)
+                val preferences = context.getSharedPreferences("currencies", MODE_PRIVATE)
+                val oldValue = preferences.getFloat(code, 0f)
+
+                val newValue = currencies!![code]!!
+                if (newValue != oldValue) preferences.edit().putFloat(code, newValue)
+
+                ItemCurrency(code = code, newValue, oldValue)
             }
         }
     }
 }
 
 @Composable
-fun ItemCurrency(code: String, value: Double) {
+fun ItemCurrency(code: String, value: Float, oldValue: Float) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -47,9 +57,12 @@ fun ItemCurrency(code: String, value: Double) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Card(backgroundColor = DarkGray, modifier = Modifier
-                .width(60.dp)
-                .fillMaxHeight(), ) {
+            Card(
+                backgroundColor = DarkGray,
+                modifier = Modifier
+                    .width(60.dp)
+                    .fillMaxHeight(),
+            ) {
                 Column(Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center) {
                     Text(
                         text = code,
@@ -63,7 +76,17 @@ fun ItemCurrency(code: String, value: Double) {
 
             Column {
                 Text(text = currenciesName[code]!!, style = MaterialTheme.typography.h5)
-                Text(text = "Значение: $value")
+                Row {
+                    Text(text = "Значение: $value")
+
+                    if (oldValue != 0f && oldValue != value) {
+                        if (oldValue > value) {
+                            Text("+${oldValue-value}", color = Color.Green)
+                        }else {
+                            Text("${oldValue-value}", color = Color.Red)
+                        }
+                    }
+                }
             }
         }
     }
