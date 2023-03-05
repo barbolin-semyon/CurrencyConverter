9ea650654154656740cc10740b3661941498bc01
barbolin-semyon <skat.apps@gmail.com>
2023-03-05 11:22:45 +0300

    Изменение импортов и удаление room

--- app/src/main/java/com/skat/currencyconverter/model/emptities/ResultApiModel.kt
+++ app/src/main/java/com/skat/currencyconverter/model/emptities/ResultApiModel.kt
@@ -1,17 +1,12 @@
 package com.skat.currencyconverter.model.emptities
 
 import android.os.Parcelable
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import com.google.gson.annotations.SerializedName
-import kotlinx.parcelize.Parcelize
+import kotlinx.android.parcel.Parcelize
 import java.util.Calendar
 
 @Parcelize
 data class ResultApiModel(
     val date: Calendar = Calendar.getInstance(),
-
-    @ColumnInfo("data")
     val data: HashMap<String, Double> = hashMapOf(),
 ) : Parcelable
 
