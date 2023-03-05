77e1632d485c25a27902919517ed82bac223eae4
barbolin-semyon <skat.apps@gmail.com>
2023-03-05 14:02:53 +0300

    Добавление ListScreen в NavGraph

--- app/src/main/java/com/skat/currencyconverter/ui/navigation/AppGraph.kt
+++ app/src/main/java/com/skat/currencyconverter/ui/navigation/AppGraph.kt
@@ -5,16 +5,18 @@ import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.skat.currencyconverter.ui.features.ListScreen
+import com.skat.currencyconverter.viewModel.CurrencyViewModel
 
 @Composable
-fun AppGraph(navController: NavHostController) {
+fun AppGraph(navController: NavHostController, viewModel: CurrencyViewModel) {
     NavHost(navController = navController, startDestination = Screens.List.route) {
         composable(Screens.Graph.route) {
 
         }
 
         composable(Screens.List.route) {
-
+            ListScreen(navController = navController, viewModel = viewModel)
         }
 
         composable(Screens.Calculator.route) {
