9b7730972f7b272f4ea73472bc9322c5b9287637
barbolin-semyon <skat.apps@gmail.com>
2023-03-05 18:17:51 +0300

    Добавление фильтра

--- app/src/main/java/com/skat/currencyconverter/model/networkService/RetrofitService.kt
+++ app/src/main/java/com/skat/currencyconverter/model/networkService/RetrofitService.kt
@@ -4,8 +4,12 @@ import com.skat.currencyconverter.model.emptities.ResultApiModel
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface RetrofitService {
     @GET("latest")
-    fun getCurrentCurrencies(@Header("apikey") apikey: String): Call<ResultApiModel>
+    fun getCurrentCurrencies(
+        @Query("base_currency") baseCurrency: String,
+        @Header("apikey") apikey: String
+    ): Call<ResultApiModel>
 }
