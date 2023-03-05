32537d27d13f61e752a129372c09f968b9dc4543
barbolin-semyon <skat.apps@gmail.com>
2023-03-05 18:18:17 +0300

    Добавление навигации и UiController

--- app/src/main/java/com/skat/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/skat/currencyconverter/MainActivity.kt
@@ -3,8 +3,14 @@ package com.skat.currencyconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.skat.currencyconverter.ui.navigation.AppGraph
 import com.skat.currencyconverter.ui.theme.CurrencyConverterTheme
+import com.skat.currencyconverter.ui.theme.DarkGray
 import com.skat.currencyconverter.viewModel.CurrencyViewModel
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +18,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CurrencyConverterTheme {
+                val uiController = rememberSystemUiController()
+                uiController.setStatusBarColor(DarkGray)
+
                 val viewModel: CurrencyViewModel = viewModel()
                 viewModel.enableListCurrencies()
+
+                val navController = rememberNavController()
+
+                AppGraph(navController = navController, viewModel = viewModel)
             }
         }
     }
